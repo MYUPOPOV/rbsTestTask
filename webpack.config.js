227f2a3ebe2ccf4e3db09cbae5e423e956d8c00c
MYuPopov <beemax@bk.ru>
2022-04-08 18:42:10 +0300

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+	context: path.resolve(__dirname, 'src'),
+
+	entry: {
+		main: './index.js',
+	},
+	output: {
+		filename: './js/[name].js',
+		path: path.join(__dirname, 'dist'),
+	},
+	devtool: 'eval-source-map',
+	devServer: {
+		hot: true,
+		static: {
+			directory: './dist',
+			watch: true,
+		},
+	},
+};
