149b806bfb90744494a0d12fcd9e362dd4d26151
MYuPopov <beemax@bk.ru>
2022-04-12 17:02:38 +0300

    Added changing Theme

--- src/modules/changeTheme.js
+++ src/modules/changeTheme.js
@@ -1,3 +1,21 @@
-const changeTheme = () => {};
+const changeTheme = () => {
+	const button = document.getElementById('change-theme-button');
+
+	const body = document.querySelector('body');
+	// const sectionBackgroundFilled = document.querySelectorAll('.section-background-filled');
+	const header = document.querySelector('.header');
+	const headerMenuThemeButton = document.querySelector('.header-menu-theme-button');
+	const i2SkillsItemBox = document.querySelector('.i2-skills-item-box');
+	const i5AboutChooseBlock = document.querySelector('.i5-about-choose-block');
+	const footer = document.querySelector('.footer');
+
+	const array = [body, sectionBackgroundFilled, header, headerMenuThemeButton, i2SkillsItemBox, i5AboutChooseBlock, footer];
+
+	button.addEventListener('click', () => {
+		array.forEach((item) => {
+			item.classList.toggle('dark-mode');
+		});
+	});
+};
 
 export default changeTheme;
