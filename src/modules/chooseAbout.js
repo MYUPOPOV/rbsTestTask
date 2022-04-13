6ff50ce3a5bab48405ad3582b5e1cbba332a7b84
MYuPopov <beemax@bk.ru>
2022-04-13 14:13:17 +0300

    Mobile adaprive flexbox

--- src/modules/chooseAbout.js
+++ src/modules/chooseAbout.js
@@ -7,12 +7,17 @@ const chooseAbout = () => {
 		if (e.target.closest('.i5-about-choose-block')) {
 			chooseBlockAll.forEach((item, index) => {
 				aboutContentAll[index].classList.add('hidden');
+				chooseBlockAll[index].classList.remove('selected');
 				if (item === e.target.closest('.i5-about-choose-block')) {
 					aboutContentAll[index].classList.toggle('hidden');
+					chooseBlockAll[index].classList.toggle('selected');
 				}
 			});
 		}
 	});
+
+	aboutContentAll[0].classList.toggle('hidden');
+	chooseBlockAll[0].classList.toggle('selected');
 };
 
 export default chooseAbout;
